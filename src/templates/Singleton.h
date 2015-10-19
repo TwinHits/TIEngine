ad66212cb59ab82f7bd7eb632f0b856a6e01d055
twinhits <twinhits@gmail.com>
2015-10-18 20:21:45 -0400

    Implemented static local variable smart pointer version of Singleton design pattern

--- src/templates/Singleton.h
+++ src/templates/Singleton.h
@@ -1,22 +1,23 @@
 #ifndef SINGLETON_H
 #define SINGLETON_H
 
+#include <memory>
+
+template <typename T>
 class Singleton
 {
 	public:
-		static Singleton& Instance()
+		static std::shared_ptr<T> Instance()
 		{
-			instance = Singleton();
+			static std::shared_ptr<T> instance;
 			return instance;
 		}
-		static Singleton instance;
 
 	protected:
 		Singleton() {}
 		Singleton(const Singleton& S) {}
 		~Singleton() {}
 		void operator=(const Singleton& S) {}
-
 };
 
 #endif
