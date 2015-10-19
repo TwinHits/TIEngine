b5ddb30d9cc64d6b9ad25b5885f0abec55f49bd4
twinhits <twinhits@gmail.com>
2015-10-18 20:21:27 -0400

    Singleton testing code

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,17 @@
 
 int main()
 {
-	SceneManager scenemanager = SceneManager();
+	std::shared_ptr<SceneManager> scenemanager1 = SceneManager::Instance();
+	std::shared_ptr<SceneManager> scenemanager2 = SceneManager::Instance();
 
-	std::cout << &scenemanager << std::endl;
+	if (scenemanager1 == scenemanager2) 
+	{
+		std::cout << "true" << std::endl;
+	}
+	else
+	{
+		std::cout << "false" << std::endl;
+	}
 
 	return 0;
 }
