69a8b3cae489f6411266f0bdb28ff06dbe1f9a18
twinhits <twinhits@gmail.com>
2015-11-01 11:11:16 -0500

    Newest stable testing render loop. Everything is in SceneManager, and I learned that if you are returning a value of any kind, you can just not assign it to a variable and let it go free.

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,18 @@
-#include <iostream>
-
 #include "managers/LogManager.h"
-#include "managers/ConfigManager.h"
-#include "managers/AssetsManager.h"
-#include "managers/HashManager.h"
-#include "managers/WindowManager.h"
 #include "managers/PlayerManager.h"
 #include "managers/SceneObjectManager.h"
 #include "managers/SceneManager.h"
 
 int main()
 {
-	//auto logmanager = LogManager::Instance();
-	//auto configmanager = ConfigManager::Instance();
-	//auto assetsmanager = AssetsManager::Instance();
-	//auto hashmanager = HashManager::Instance();
-	//auto windowmanager = WindowManager::Instance();
-	auto playermanager = PlayerManager::Instance();
-	//auto sceneobjectmanager = SceneObjectManager::Instance();
-	//auto scenemanager = SceneManager::Instance();
-
-	const Player& player = playermanager->addPlayer();
-	sf::RenderWindow& window = player.getWindow();
-
-	sf::Sprite test;
+	PlayerManager::Instance()->addPlayer();
+	SceneObjectManager::Instance()->addSceneObject();
 
 	while (true)
 	{
-		window.clear();
-		window.draw(test);
-		window.display();
+		//ProcessInput
+		//UpdateGameState
+		SceneManager::Instance()->render();		
 	}
 
 	return 0;
