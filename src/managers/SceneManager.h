33f0ebb3bb1ef5a1fe4207d15c1fb465f37df897
twinhits <twinhits@gmail.com>
2015-11-01 11:10:33 -0500

    Added SceneManager::render() which gathers all players and all sceneobjets and renders each sceneobject into each player's window.

--- src/managers/SceneManager.h
+++ src/managers/SceneManager.h
@@ -6,7 +6,7 @@
 class SceneManager : public Singleton<SceneManager>
 {
 	public:
-		void draw();
+		void render();
 		~SceneManager();
 		SceneManager();
 	private:
--- src/managers/SceneManagerImplementation.cpp
+++ src/managers/SceneManagerImplementation.cpp
@@ -1,10 +1,26 @@
 #include "SceneManager.h"
+#include "PlayerManager.h"
+#include "SceneObjectManager.h"
+
 
 SceneManager::SceneManager() {}
 	
 SceneManager::~SceneManager() {}
 
-void SceneManager::draw()
-{
+void SceneManager::render()
+{		
+	const std::map<unsigned long, Player*>& players = PlayerManager::Instance()->getAllPlayers();
+	const std::map<unsigned long, SceneObject*>& sceneObjects = SceneObjectManager::Instance()->getAllSceneObjects();
 
+	for (auto p = players.begin(); p != players.end(); ++p)
+	{
+		sf::RenderWindow& window = p->second->getWindow();
+		window.clear();
+		for (auto so = sceneObjects.begin(); so != sceneObjects.end(); ++so)
+		{
+			window.draw(so->second->getSprite());
+		}
+		window.display();
+	}
+	
 }
