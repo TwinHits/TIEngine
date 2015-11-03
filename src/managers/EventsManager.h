7cecc73af6902dcc1127cc2e880d11d7bb600a3e
twinhits <twinhits@gmail.com>
2015-11-03 17:08:49 -0500

    Added eventsmanager to manage keybindings, and hopefully the commands that are bound to them.

--- src/managers/EventsManager.h
+++ src/managers/EventsManager.h
@@ -1,17 +1,24 @@
 #ifndef EVENTSMANAGER_H
 #define EVENTSMANAGER_H
 
+#include <map>
+
+#include <SFML/Graphics.hpp>
+
 #include "../templates/Singleton.h"
 
 class EventsManager : public Singleton<EventsManager>
 {
 	public:
+		void processInput(const sf::Event& event);
+
 		EventsManager();
 		~EventsManager();
 	private:
+		std::map<unsigned int, unsigned int> keyBinds;
+
 		EventsManager(const EventsManager&);
 		void operator=(const EventsManager&);
-
 };
 
 #endif
--- src/managers/EventsManagerImplementation.cpp
+++ src/managers/EventsManagerImplementation.cpp
@@ -1,5 +1,67 @@
+#include <iostream>
+
 #include "EventsManager.h"
 
-EventsManager::EventsManager() {}
+EventsManager::EventsManager()
+{
+	/*
+	keyBinds[sf::Keyboard::0] = 0;
+	keyBinds[sf::Keyboard::1] = 1;
+	keyBinds[sf::Keyboard::2] = 2;
+	keyBinds[sf::Keyboard::3] = 3;
+	keyBinds[sf::Keyboard::4] = 4;
+	keyBinds[sf::Keyboard::5] = 5;
+	keyBinds[sf::Keyboard::6] = 6;
+	keyBinds[sf::Keyboard::7] = 7;
+	keyBinds[sf::Keyboard::8] = 8;
+	keyBinds[sf::Keyboard::9] = 9;
+	*/
+	keyBinds[sf::Keyboard::A] = 10;
+	keyBinds[sf::Keyboard::B] = 11;
+	keyBinds[sf::Keyboard::C] = 12;
+	keyBinds[sf::Keyboard::D] = 13;
+	keyBinds[sf::Keyboard::E] = 14;
+	keyBinds[sf::Keyboard::F] = 15;
+	keyBinds[sf::Keyboard::G] = 16;
+	keyBinds[sf::Keyboard::H] = 17;
+	keyBinds[sf::Keyboard::I] = 18;
+	keyBinds[sf::Keyboard::J] = 19;
+	keyBinds[sf::Keyboard::K] = 20;
+	keyBinds[sf::Keyboard::L] = 21;
+	keyBinds[sf::Keyboard::M] = 22;
+	keyBinds[sf::Keyboard::N] = 23;
+	keyBinds[sf::Keyboard::O] = 24;
+	keyBinds[sf::Keyboard::P] = 25;
+	keyBinds[sf::Keyboard::Q] = 26;
+	keyBinds[sf::Keyboard::R] = 27;
+	keyBinds[sf::Keyboard::S] = 28;
+	keyBinds[sf::Keyboard::T] = 29;
+	keyBinds[sf::Keyboard::U] = 30;
+	keyBinds[sf::Keyboard::V] = 31;
+	keyBinds[sf::Keyboard::W] = 32;
+	keyBinds[sf::Keyboard::X] = 33;
+	keyBinds[sf::Keyboard::Y] = 34;
+	keyBinds[sf::Keyboard::Z] = 35;
+	keyBinds[sf::Keyboard::Escape] = 36;
+
+}
 EventsManager::~EventsManager() {}
 
+void EventsManager::processInput(const sf::Event& event)
+{
+	//1) How do I condfigure a link to a function? 
+	//2) How can I register a function without modifyting the eventmanager src
+	//code?
+	//3) How does the event manager know which window/player/object to operate
+	//on?
+
+	switch (event.type)
+	{
+		case sf::Event::KeyPressed:
+			std::cout << keyBinds[event.key.code] << std::endl;
+			break;	
+		default:
+			break;
+	}
+
+}
