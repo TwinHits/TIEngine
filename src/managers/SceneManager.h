e616a92499fc2228c8e1aa0b5950cded3a7dee54
twinhits <twinhits@gmail.com>
2015-11-08 11:14:01 -0500

    Added getFPS() that returns the number of frames determined by a frame by frame basis.

--- src/managers/SceneManager.h
+++ src/managers/SceneManager.h
@@ -1,16 +1,22 @@
 #ifndef SCENEMANAGER_H
 #define SCENEMANGER_H
 
+#include "TimeManager.h"
+
 #include "../templates/Singleton.h"
 
 class SceneManager : public Singleton<SceneManager>
 {
 	public:
 		void render();
+		int getFPS();
 
 		~SceneManager();
 		SceneManager();
 	private:
+		sf::Clock clock;
+		int fps;
+
 		SceneManager(const SceneManager&);
 		void operator=(const SceneManager&);
 };
--- src/managers/SceneManagerImplementation.cpp
+++ src/managers/SceneManagerImplementation.cpp
@@ -1,13 +1,18 @@
 #include "SceneManager.h"
 #include "WindowManager.h"
 #include "SceneObjectManager.h"
+#include "TimeManager.h"
 
-SceneManager::SceneManager() {}
+SceneManager::SceneManager() 
+{
+	this->clock = TimeManager::Instance()->addClock();
+}
 	
 SceneManager::~SceneManager() {}
 
 void SceneManager::render()
 {		
+	
 	sf::RenderWindow& window = WindowManager::Instance()->getWindow();
 	const std::map<unsigned long, SceneObject*>& sceneObjects = SceneObjectManager::Instance()->getAllSceneObjects();
 
@@ -17,4 +22,13 @@ void SceneManager::render()
 		window.draw(so->second->getSprite());
 	}
 	window.display();
+
+	double frame = clock.restart().asSeconds();
+	fps = 100 / frame;
+
+}
+
+int SceneManager::getFPS()
+{
+	return fps;	
 }
