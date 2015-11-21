4485fa767af56ca00a366bb9120e7957159dc272
twinhits <twinhits@gmail.com>
2015-11-20 22:32:09 -0500

    Added displayLanguage to config.ini.

--- src/managers/ConfigManager.h
+++ src/managers/ConfigManager.h
@@ -6,6 +6,8 @@
 
 #include "../templates/Singleton.h"
 
+#include "../objects/Language.h"
+
 class ConfigManager: public Singleton<ConfigManager>
 {
 	public:
@@ -15,15 +17,18 @@ class ConfigManager: public Singleton<ConfigManager>
 		const std::string& getDebugLogPath();
 		const std::string& getAssetsPath();
 		const int& getDebugLogLevel();
+		const Language& getDefaultDisplayLanguage();
 			
 	private:
 		void loadConfig();
 		void parseConfig(std::ifstream& config);
+		Language parseLanguage(const std::string& s);
 
 		//Configurable Values
 		std::string debugLogPath = "";
 		std::string assetsPath = "";
 		int debugLogLevel = 3;
+		Language defaultDisplayLanguage = EN;
 
 		ConfigManager(const ConfigManager&);
 		void operator=(const ConfigManager&);
--- src/managers/ConfigManagerImplementation.cpp
+++ src/managers/ConfigManagerImplementation.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <map>
 
 #include <boost/filesystem.hpp>
 
@@ -18,8 +18,7 @@ void ConfigManager::loadConfig()
 {
 	auto configPath = boost::filesystem::path("config.ini");
 	if (exists(configPath))
-	{
-		std::ifstream config;
+	{ std::ifstream config;
 
 		config.open("config.ini");
 		if (!config.is_open())
@@ -46,14 +45,34 @@ void ConfigManager::parseConfig(std::ifstream& config)
 		{
 			std::getline(fileline, value);	
 
-			//To add a configurable value, add type to header and register here
 			if (key == "DebugLogPath") { debugLogPath = value; }
 			if (key == "AssetsPath") { assetsPath = value; }
 			if (key == "DebugLogLevel") { debugLogLevel = StringToNumber(value); }
+			if (key == "defaultDisplayLanguage") { defaultDisplayLanguage = parseLanguage(value); }
 		}
 	}
+}
+
+Language ConfigManager::parseLanguage(const std::string& s)
+{
+	std::map<std::string, Language> languages;
+	languages["EN"] = EN;
+	languages["ES"] = ES;
+	languages["FR"] = FR;
+	languages["DE"] = DE;
+	languages["JP"] = JP;
+	languages["RU"] = RU;
+	languages["AR"] = AR;
+	languages["CN"] = CN;
+
+	Language l = languages[s];
+	if (l == 0)
+		return EN;
+	else
+		return l;
 }
 
 const std::string& ConfigManager::getDebugLogPath() { return debugLogPath; }
 const std::string& ConfigManager::getAssetsPath() { return assetsPath; }
 const int& ConfigManager::getDebugLogLevel() { return debugLogLevel; }
+const Language& ConfigManager::getDefaultDisplayLanguage() { return defaultDisplayLanguage; }
