12b20668d30809d843e39d50b6129722bc143ad0
twinhits <twinhits@gmail.com>
2016-03-28 22:47:52 -0400

    Added option to choose if you want to return a ref or a ptr.

--- src/templates/Factory.h
+++ src/templates/Factory.h
@@ -11,16 +11,27 @@ namespace TIE
 	class Factory
 	{
 		public:
-			const T& create() 
+			const T& create_ref() 
 			{ 
 				T* ptr = new T();
 				const T& ref = dynamic_cast<const T&>(SceneObjectManager::Instance()->addSceneObject(ptr));
 				return ref;
 			}
-			const T& create(T* ptr)
+			const T& create_ref(T* ptr)
 			{
 				const T& ref = dynamic_cast<const T&>(SceneObjectManager::Instance()->addSceneObject(ptr));
 				return ref;
+			}
+			const T* create_ptr() 
+			{ 
+				const T* ptr = new T();
+				SceneObjectManager::Instance()->addSceneObject(ptr);
+				return ptr;
+			}
+			const T* create_ptr(T* ptr)
+			{
+				SceneObjectManager::Instance()->addSceneObject(ptr);
+				return ptr;
 			}
 	
 		protected:
