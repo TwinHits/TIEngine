ce93382d90a8dc5cb3fd9cb38826102c874a785b
twinhits <twinhits@gmail.com>
2016-04-15 10:42:04 -0400

    Initial definitions of Assets Management tables.

---
+++ sql/AssetsManagement.sql
@@ -0,0 +1,7 @@
+CREATE TABLE TIE_Images (
+	imageId serial PRIMARY KEY NOT NULL,
+	imagePath text             NOT NULL);
+
+CREATE TABLE TIE_Audio (
+	imageId serial PRIMARY KEY NOT NULL,
+	imagePath text             NOT NULL);
