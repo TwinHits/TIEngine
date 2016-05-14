58098e44b78dc523290d01986ae53839feccbedc
twinhits <twinhits@gmail.com>
2016-05-14 14:42:55 -0400

    Adjusted StringManager to handle new single return from DatabaseManager.

--- src/managers/StringManagerImplementation.cpp
+++ src/managers/StringManagerImplementation.cpp
@@ -22,17 +22,9 @@ std::string StringManager::getString(const GlobalId id, const Language l)
 {
 
 	std::string query = "SELECT COALESCE((SELECT localizedContent FROM TIE_StringLocalizations WHERE _languageId=" + std::to_string(l) + "), defaultContent) FROM TIE_LanguageStrings LEFT JOIN TIE_StringLocalizations ON stringId=_stringId WHERE stringId =" + std::to_string(id) + " LIMIT 1;";
-	Result result;
-	DatabaseManager::Instance()->Select(query, result);
-	
+
 	std::string s;
-	for (auto i : result)
-	{
-		for (auto j : i)
-		{
-			s = j.c_str();
-		}
-	}
+	DatabaseManager::Instance()->Select(query, s);
 
 	return s;
 }
