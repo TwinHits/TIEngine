f88e47afdc0d603eb407cae19ac8c644dde50ba7
John Goben <John Goben@TWINHITS>
2018-04-02 14:30:38 -0400

    Wrote unit tests for ConfigManager, providing a test config.ini file to use.

--- tests/ConfigManager.cpp
+++ tests/ConfigManager.cpp
@@ -1,43 +1,59 @@
 #include "pch.h"
 
-//Calling for a configuration value without providing a configuration file and when 
-//we can't find the default configuration file should return a default value
-TEST(ConfigManager, GetDebugLogPath_DefaultPath) {
-	//reset to default path
-	TIE::ConfigManager::Instance()->loadConfigFile("config.ini");
-
-	std::string expected = "/logs";
+//Test that when using a config file we cannot find that the default values are returned correctly.
+TEST(ConfigManager, GetDebugLogPathDefault) {
+	std::string expected = "logs/";
 	std::string actual = TIE::ConfigManager::Instance()->getDebugLogPath();
 	
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(ConfigManager, GetAssetsPath_DefaultPath) {
-	//reset to default path
-	TIE::ConfigManager::Instance()->loadConfigFile("config.ini");
-
-	std::string expected = "/assets";
+TEST(ConfigManager, GetAssetsPathDefault) {
+	std::string expected = "assets/";
 	std::string actual = TIE::ConfigManager::Instance()->getAssetsPath();
 
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(ConfigManager, GetDebugLogLevel_DefaultPath) {
-	//reset to default path
-	TIE::ConfigManager::Instance()->loadConfigFile("config.ini");
-
+TEST(ConfigManager, GetDebugLogLevelDefault) {
 	int expected = 3;
 	int actual = TIE::ConfigManager::Instance()->getDebugLogLevel();
 
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(ConfigManager, GetDefaultDisplayLanguage_DefaultPath) {
-	//reset to default path
-	TIE::ConfigManager::Instance()->loadConfigFile("config.ini");
-
+TEST(ConfigManager, GetDefaultDisplayLanguageDefault) {
 	Language expected = en_US;
 	Language actual = TIE::ConfigManager::Instance()->getDefaultDisplayLanguage();
 
 	ASSERT_EQ(expected, actual);
+}
+
+//Test when given a specific configuration file, the file is loaded correctly.
+TEST(ConfigManager, LoadCustomConfigFile) {
+	std::string testConfigurationFile = "../../tests/testconfig.ini";
+	bool success = TIE::ConfigManager::Instance()->loadConfigFile(testConfigurationFile);
+	ASSERT_TRUE(success);
+}
+
+//Test parsing the configuration values from a config file
+TEST(ConfigManager, ParseConfigFile) {
+	std::string testConfigurationFile = "../../tests/testconfig.ini";
+	bool success = TIE::ConfigManager::Instance()->loadConfigFile(testConfigurationFile);
+
+	std::string expectedAssets = "tests/assets/";
+	std::string actualAssets = TIE::ConfigManager::Instance()->getAssetsPath();
+	ASSERT_EQ(expectedAssets, actualAssets);
+
+	std::string expectedLogs = "tests/logs/";
+	std::string actualLogs = TIE::ConfigManager::Instance()->getDebugLogPath();
+	ASSERT_EQ(expectedLogs, actualLogs);
+
+	int expectedDebugLogLevel = 1;
+	int actualDebugLogLevel = TIE::ConfigManager::Instance()->getDebugLogLevel();
+	ASSERT_EQ(expectedDebugLogLevel, actualDebugLogLevel);
+
+	Language expectedDefaultDisplayLanguage = ja_JP;
+	Language actualDefaultDisplayLanguage = TIE::ConfigManager::Instance()->getDefaultDisplayLanguage();
+	ASSERT_EQ(expectedDefaultDisplayLanguage, actualDefaultDisplayLanguage);
 }
