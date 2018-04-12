e05095a9d54773162e111a03b0b34303e4240c2b
John Goben <John Goben@TWINHITS>
2018-04-12 00:03:20 -0400

    Fixed long unisgned conversion warning message.

--- tests/GlobalId.cpp
+++ tests/GlobalId.cpp
@@ -5,7 +5,8 @@ TEST(GlobalId, GlobalIdIsAnUnsignedLong) {
 	TIE::GlobalId max = ULONG_MAX;
 	TIE::GlobalId min = 0;
 	TIE::GlobalId neg = LONG_MIN;
+	TIE::GlobalId pos = LONG_MAX+1;
 	ASSERT_EQ(max, ULONG_MAX);
 	ASSERT_EQ(min, 0);
-	ASSERT_EQ(neg, -neg);
+	ASSERT_EQ(neg, pos);
 }
