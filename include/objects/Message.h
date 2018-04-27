738a954cd43d0d7239e8fbeea46fb38936947bd4
John Goben <John Goben@TWINHITS>
2018-04-26 20:25:29 -0400

    Refactored Message.h.

--- include/objects/Message.h
+++ include/objects/Message.h
@@ -5,41 +5,36 @@
 #include "TIEntity.h"
 #include "GlobalId.h"
 
-namespace TIE
-{
+namespace TIE {
 
-	class TIEntity;
+class TIEntity;
 
-	class Message 
-	{
-		public:
-			Message(GlobalId s, GlobalId r);
-			Message(GlobalId s);
+class Message {
+	public:
+		Message(GlobalId s, GlobalId r);
+		Message(GlobalId s);
 
-			struct MoveMsg 
-			{
-				//msg.move.dest
-				//Move specific data
-				int dest;
-			};
+		struct MoveMsg {
+			//msg.move.dest
+			//Move specific data
+			int dest;
+		};
 		
-			enum MessageType
-		   	{
-				//Possible messages to send;
-				Move
-			};
+		enum MessageType {
+			//Possible messages to send;
+			Move
+		};
 			
-			MessageType type;
-			GlobalId senderId;
-			GlobalId receiverId;	
-		
-			union 
-			{
-				//Registry of the possible messages?
-				MoveMsg move;
-			};
+		MessageType type;
+		GlobalId senderId;
+		GlobalId receiverId;	
+	
+		union {
+			//Registry of the possible messages?
+			MoveMsg move;
+		};
 
-	};
+};
 
 }
 #endif
