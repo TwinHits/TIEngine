1259f7a45dbe801ad01df40f7033a3166d836e6d
John Goben <John Goben@TWINHITS>
2018-05-01 14:09:46 -0400

    Second fix for Long warning.

--- tests/GlobalId.cpp
+++ tests/GlobalId.cpp
@@ -4,9 +4,6 @@
 TEST(GlobalId, GlobalIdIsAnUnsignedLong) {
 	TIE::GlobalId max = ULONG_MAX;
 	TIE::GlobalId min = 0;
-	TIE::GlobalId neg = LONG_MIN;
-	TIE::GlobalId pos = LONG_MAX;
 	ASSERT_EQ(max, ULONG_MAX);
 	ASSERT_EQ(min, 0);
-	ASSERT_EQ(neg, pos);
 }
