6818510e4d9f6d30fa595cd9d3a8f78736a2aa36
TwinHits <TwinHits@gmail.com>
2020-07-05 21:53:01 -0400

    Migrated get missing text to text component.

--- src/objects/components/TextComponent.cpp
+++ src/objects/components/TextComponent.cpp
@@ -1,11 +1,15 @@
 #include "objects/components/TextComponent.h"
 
-#include "utilities/GetMissingText.h"
+#include "managers/AssetsManager.h"
 
 using namespace TIE;
 
 TextComponent::TextComponent() {
-	//Move to get as a future optimization
+	const sf::Font& font = TIE::AssetsManager::Instance()->getFont("font.tff");
+	this->setString("NO_TEXT");
+	this->setFont(font);
+	this->setCharacterSize(14);
+	this->setPosition(sf::Vector2f(0, 0));
 }
 
 void TextComponent::setDrawn(bool drawn) {
