b9d3aa33839dca2fbc557af8afd0fb552e25c285
TwinHits <TwinHits@gmail.com>
2020-07-05 21:56:20 -0400

    Wrote TIEntity factory for script manager to use for generic entity creation.

---
+++ include/objects/factories/TIEntityFactory.h
@@ -0,0 +1,51 @@
+#ifndef TIENTITYFACTORY_H
+#define TIENTITYFACTORY_H
+
+#include <string>
+
+#include "objects/entities/TIEntity.h" 
+
+namespace TIE {
+
+class TIEntityFactory {
+	public:
+		TIEntity& build();
+
+		TIEntityFactory& setParent(TIEntity*);
+		TIEntityFactory& setName(std::string);
+
+		TIEntityFactory& setDrawn(bool);
+		TIEntityFactory& setTexture(const std::string&);
+		TIEntityFactory& setText(const std::string&);
+
+		TIEntityFactory& setSpeed(const float);
+		TIEntityFactory& setDirection(const float);
+
+		TIEntityFactory() {};
+		~TIEntityFactory() {};
+
+		static const std::string DRAWN;
+		static const std::string TEXTURE;
+		static const std::string TEXT;
+
+		static const std::string MOVES;
+		static const std::string SPEED;
+		static const std::string DIRECTION;
+
+	private:
+		TIEntity* parent = nullptr;
+		std::string name = "";
+
+		bool hasSprite = false;
+		bool hasText = false;
+		bool isDrawn = false;
+		std::string texture = "";
+		std::string text = "";
+
+		bool hasMoves = false;
+		float speed = 0;
+		float direction = 0;
+};
+
+}
+#endif
---
+++ src/objects/factories/TIEntityFactory.cpp
@@ -0,0 +1,95 @@
+#include "objects/factories/TIEntityFactory.h"
+
+#include "managers/AssetsManager.h"
+#include "managers/LogManager.h"
+#include "managers/SceneManager.h"
+#include "objects/components/SpriteComponent.h"
+#include "objects/components/TextComponent.h"
+#include "objects/components/MovesComponent.h"
+#include "objects/entities/TIEntity.h"
+
+
+using namespace TIE;
+
+const std::string TIEntityFactory::DRAWN = "drawn";
+const std::string TIEntityFactory::TEXTURE = "texture";
+const std::string TIEntityFactory::TEXT = "text";
+const std::string TIEntityFactory::MOVES = "moves";
+const std::string TIEntityFactory::SPEED = "speed";
+const std::string TIEntityFactory::DIRECTION = "direction";
+
+TIEntity& TIEntityFactory::build() {
+
+	if (this->parent == nullptr) {
+		this->parent = &SceneManager::Instance()->getClientLayer();
+	}
+
+	TIEntity& tientity = this->parent->attachChild();
+
+	if (this->hasSprite) {
+		SpriteComponent* spriteComponent = tientity.addComponent<SpriteComponent>();
+		spriteComponent->setDrawn(this->isDrawn);
+		const sf::Texture& texture = AssetsManager::Instance()->getTexture(this->texture);
+		spriteComponent->setTexture(texture);
+		spriteComponent->setTextureRect(sf::IntRect(sf::Vector2i(0, 0), sf::Vector2i(texture.getSize())));
+	}
+
+	if (this->hasText) {
+		TextComponent* textComponent = tientity.addComponent<TextComponent>();
+		textComponent->setDrawn(this->isDrawn);
+		textComponent->setString(this->text);
+	}
+
+	if (this->hasMoves) {
+		MovesComponent* movesComponent = tientity.addComponent<MovesComponent>();
+		movesComponent->setVelocity(sf::Vector2f(this->speed, this->direction));
+		if (!this->hasSprite) {
+			LogManager::Instance()->warn("Entity " + this->name + " has a move component but no sprite component.");
+		}
+	}
+
+	return tientity;
+}
+
+TIEntityFactory& TIEntityFactory::setParent(TIEntity* parent) {
+	this->parent = parent;
+	return *this;
+}
+
+TIEntityFactory& TIEntityFactory::setName(std::string name) {
+	this->name = name;
+	return *this;
+}
+
+TIEntityFactory& TIEntityFactory::setDrawn(bool drawn) {
+	this->isDrawn = drawn;
+	return *this;
+}
+
+
+TIEntityFactory& TIEntityFactory::setTexture(const std::string& texture) {
+	this->texture = texture;
+	this->hasSprite = true;
+	return *this;
+}
+
+
+TIEntityFactory& TIEntityFactory::setText(const std::string& text) {
+	this->text = text;
+	this->hasText = true;
+	return *this;
+}
+
+
+TIEntityFactory& TIEntityFactory::setSpeed(const float speed) {
+	this->speed = speed;
+	this->hasMoves = true;
+	return *this;
+}
+
+
+TIEntityFactory& TIEntityFactory::setDirection(const float direction) {
+	this->direction = direction;
+	this->hasMoves = true;
+	return *this;
+}
