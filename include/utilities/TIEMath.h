3be15fcb880b9ef6abe4c2bfe70c540d738f5ce8
John Goben <TwinHits@gmail.com>
2020-08-07 17:04:43 -0400

    #50 Added more supporting math methods.

--- include/utilities/TIEMath.h
+++ include/utilities/TIEMath.h
@@ -8,12 +8,14 @@
 
 namespace TIE {
 	namespace Math {
-		float toRadians(float);
-		float toDegrees(double);
+		float toRadians(const float);
+		float toDegrees(const double);
 		float distanceBetweenTwoPoints(const sf::Vector2f&, const sf::Vector2f&);
 		float angleBetweenTwoPoints(const sf::Vector2f&, const sf::Vector2f&);
-		sf::Vector2f translateVelocityByTime(const sf::Vector2f&, float);
+		float convertTo360Degrees(const float);
+		sf::Vector2f translateVelocityByTime(const sf::Vector2f&, const float);
 		sf::Vector2f normalizePositionToGrid(const sf::Vector2f& position, const sf::FloatRect& bounds, const sf::Vector2f& tileSize);
+		const int directionFromAngleToAngle(const float, const float);
 		bool isNice(int);
 	}
 }
--- src/utilities/TIEMath.cpp
+++ src/utilities/TIEMath.cpp
@@ -17,11 +17,15 @@ float TIE::Math::distanceBetweenTwoPoints(const sf::Vector2f& p1, const sf::Vect
 
 
 float TIE::Math::angleBetweenTwoPoints(const sf::Vector2f& p1, const sf::Vector2f& p2) {
-	return TIE::Math::toDegrees(std::atan2(p2.y - p1.y, p2.x - p1.x));
+	return TIE::Math::convertTo360Degrees(TIE::Math::toDegrees(std::atan2(p2.y - p1.y, p2.x - p1.x)));
+}
+
+float TIE::Math::convertTo360Degrees(const float degrees) {
+	return (static_cast<int>(degrees) + 360) % 360;
 }
 
 
-sf::Vector2f TIE::Math::translateVelocityByTime(const sf::Vector2f& velocity, float time) {
+sf::Vector2f TIE::Math::translateVelocityByTime(const sf::Vector2f& velocity, const float time) {
 	return sf::Vector2f(std::cos(TIE::Math::toRadians(velocity.y)) * velocity.x * time, std::sin(TIE::Math::toRadians(velocity.y)) * velocity.x * time);
 }
 
@@ -31,6 +35,15 @@ sf::Vector2f TIE::Math::normalizePositionToGrid(const sf::Vector2f& position, co
 		bounds.left + floor(fabsf(bounds.left - position.x) / tileSize.x) * tileSize.x + tileSize.x / 2,
 		bounds.top + floor(fabsf(bounds.top - position.y) / tileSize.y) * tileSize.y + tileSize.y / 2
 	);
+}
+
+
+const int TIE::Math::directionFromAngleToAngle(const float start, const float end) {
+	if (abs(start -end) >= 180) {
+		return 1;
+	} else {
+		return 1;
+	}
 }
 
 
