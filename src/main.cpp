81627ddbbd1b1fc9982d5a21b6fb9a00e727a4ec
goben.john@gmail.com <goben.john@gmail.com>
2020-11-28 14:18:39 -0500

    #89 Removed TIEngine.h header, since the engine is no longer a library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
-#include <TIEngine.h>
+/* This is where the lisence agreement would go.
+ *
+ * This is Twin Ion Engine, a game engine for the SFML graphics library.
+ * Include this file, in order to access all the neccessary classes to utilize
+ * this libray.
+ * * See src/main.cpp for an example of the basic render loop.
+ *
+ * Glory and gore go hand in hand.
+*/
+
+#include "managers/EventsManager.h"
+#include "managers/SceneManager.h"
+#include "managers/StartUpManager.h"
+#include "managers/WindowManager.h"
 
 int main() {
 	TIE::StartUpManager::Instance()->initialize();
