58c0380cf018f8805a6b8d174ef3d01384eadc0e
goben.john@gmail.com <goben.john@gmail.com>
2024-02-18 23:47:56 -0500

    Added UIElement and Button graphical implementation.

---
+++ include/objects/tientities/ui/UIElement.h
@@ -0,0 +1,19 @@
+#ifndef UIELEMENT_H
+#define UIELEMENT_H
+
+#include "objects/tientities/TIEntity.h"
+
+namespace TIE {
+
+class UIElement : public TIEntity {
+    public:
+        UIElement() {};
+        ~UIElement() {};
+
+    private:
+
+};
+
+}
+
+#endif
---
+++ src/objects/tientities/ui/Button.cpp
@@ -0,0 +1,33 @@
+#include "objects/tientities/ui/Button.h"
+
+#include <SFML/Graphics.hpp>
+
+#include "componentsystems/MessagesComponentSystem.h"
+#include "componentsystems/PositionComponentSystem.h"
+#include "componentsystems/ShapeComponentSystem.h"
+#include "componentsystems/TextComponentSystem.h"
+#include "managers/MessageManager.h"
+#include "objects/GlobalId.h"
+#include "objects/enumeration/TextAlignment.h"
+
+using namespace TIE;
+
+Button::Button(const std::string text, const sf::Vector2f& position, const sf::Vector2f& size) {
+    this->setName(text + " Button");
+
+    PositionComponent& positionComponent = PositionComponentSystem::Instance()->addComponent(*this);
+    positionComponent.position = position;
+
+    ShapeComponent& shapeComponent = ShapeComponentSystem::Instance()->addComponent(*this);
+    sf::RectangleShape& rectangleShape = shapeComponent.addRectangleShape();
+    rectangleShape.setSize(size);
+    rectangleShape.setOrigin(sf::Vector2f(size.x / 2, size.y / 2));
+    rectangleShape.setOutlineThickness(2);
+    rectangleShape.setOutlineColor(sf::Color::White);
+    rectangleShape.setFillColor(sf::Color::Transparent);
+
+    TextComponent& textComponent = TextComponentSystem::Instance()->addComponent(*this);
+    textComponent.setTextAlignment(TextAlignment::CENTER);
+    textComponent.setString(text);
+    textComponent.setCharacterSize(16);
+}
